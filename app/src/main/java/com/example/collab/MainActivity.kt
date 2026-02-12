180771e861e47afcb82763e66d415138c2d8d8c2
FrancesAganan <francesaganan1@gmail.com>
2026-02-12 16:08:29 +0800

    comment

--- app/src/main/java/com/example/collab/MainActivity.kt
+++ app/src/main/java/com/example/collab/MainActivity.kt
@@ -12,7 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.collab.ui.theme.CollabTheme
-
+//fdsfsdfsffsdfdsf
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
